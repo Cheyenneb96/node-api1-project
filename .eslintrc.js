cf89dc278129b0c6b5e3b3665a04968f0719d045
Cheyenne Bowman <cheyennebowman1212123@gmail.com>
2021-11-29 14:58:57 -0600

    not breaking

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 13
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+};
